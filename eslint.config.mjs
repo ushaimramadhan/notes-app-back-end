87f0f06c0c33749fa9142a814b1220349b6b511a
Ushaim Ramadhan F <adhan1005@gmail.com>
2024-12-07 14:24:12 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  daStyle,
+  // other config style
+];
